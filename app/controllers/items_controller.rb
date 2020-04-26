4dc379fd428db979b4f711d79cf5c00915ea51f5
m.teshita <masato.t2207@gmail.com>
2020-04-26 17:46:41 +0900

    商品出品に必要なアクションの定義
    newアクション、createアクションを定義。privateメソッドとしてitem_paramsを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def new
+    @item = Item.new
+    @item.images.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(Item.column_names.map{|c| c.to_sym})
   end
 end
