e3afdc48956909604f1d36fd533967d4fe8f0565
m.teshita <masato.t2207@gmail.com>
2020-04-27 22:51:07 +0900

    出品機能の動作確認用にindexアクションのルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tops, only: [:new]
-  resources :items, only: [:new, :create]
+  resources :items, only: [:index, :new, :create]
 end
