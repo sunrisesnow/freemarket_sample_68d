fddcf670b759acae11e4e2102c212d2d831ec1bc
m.teshita <masato.t2207@gmail.com>
2020-05-02 23:46:13 +0900

    active_hash適用のための変更
    ・active_hashで使用するモデルとのバリデーションの設定
    ・itemsテーブル自体には、各モデルのidを使用するので、バリデーションのカラム名を変更
    ・不要な記述を削除（エラーメッセージはjqueryで行うため）

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,25 +3,25 @@ class Item < ApplicationRecord
   # belongs_to :saler, class_name: User
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-  
-  # 画像を1枚以上登録するバリデーション
-  validate :require_any_image
-  def require_any_image
-    errors.add(:base, :no_image)
-  end
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_date
+  belongs_to_active_hash :delivery_method
+  belongs_to_active_hash :trading_status
   
   # 入力必須のバリデーション
   with_options presence: true do
     validates :name
     validates :explanation
-    validates :status
+    validates :status_id
     validates :delivery_charge_flag
-    validates :prefectures
-    validates :delivery_date
+    validates :prefecture_id
+    validates :delivery_date_id
     validates :price
   end
 
-  # 販売価格の上限のバリデーション
+  # 販売価格の数値範囲のバリデーション
   validates :price, numericality: {greater_than_or_equal_to: 300}
   validates :price, numericality: {less_than: 10000000}
 end
