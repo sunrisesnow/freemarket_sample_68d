4400fdf8323b55611b6d7eee75b204e980763fe5
dolce <ex193700@yahoo.co.jp>
2020-05-03 16:15:09 +0900

    stylesheet作成

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,6 +1,9 @@
 class ItemController < ApplicationController
   before_action :set_item, only: [:show]
  def index
+  @items = Item.where(status: false).limit(3).order("created_at DESC")
+  @images = Image.all
+  @categories = Category.all
  end
 
  def new
