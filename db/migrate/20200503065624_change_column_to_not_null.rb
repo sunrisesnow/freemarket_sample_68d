edc14f6c7571a53ddbdd735ad21a971e9d654def
m.teshita <masato.t2207@gmail.com>
2020-05-03 20:02:25 +0900

    本番環境でのmigrate時に矛盾を発生させないように調整

--- db/migrate/20200503065624_change_column_to_not_null.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnToNotNull < ActiveRecord::Migration[5.2]
-  def up
-    change_column :items, :saler_id, :integer, null: true
-  end
-
-  def down
-    change_column :items, :saler_id, :integer, null: false
-  end
-end
