40827e90df32ca7cc18f34a42e130aa2548b3b13
m.teshita <masato.t2207@gmail.com>
2020-05-03 16:10:49 +0900

    Update 20200503065624_change_column_to_not_null.rb

--- db/migrate/20200503065624_change_column_to_not_null.rb
+++ db/migrate/20200503065624_change_column_to_not_null.rb
@@ -1,11 +1,11 @@
 class ChangeColumnToNotNull < ActiveRecord::Migration[5.2]
   def change
     def up
-      change_column :imtes, :saler_id, :string, null: true
+      change_column :imtes, :saler_id, :integer, null: true
     end
   
     def down
-      change_column :items, :saler_id, :string, null: false
+      change_column :items, :saler_id, :integer, null: false
     end
   end
 end
