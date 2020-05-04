04f9a9fa1d35f73f04bce98cbfde23c36930e78b
dolce <ex193700@yahoo.co.jp>
2020-05-04 10:58:45 +0900

    view修正

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,32 @@
+class ItemsController < ApplicationController
+  #before_action :set_item, only: [:show]
+  def index
+   @items = Item.where(status: false).limit(3).order("created_at DESC")
+   @images = Image.all
+   #@categories = Category.all
+  end
+ 
+  def new
+  end
+ 
+  def create
+  end
+ 
+  def edit
+  end
+ 
+  def destroy
+  end
+ 
+  def show
+   #@images = @items.images
+   #@categories = Category.all
+  end
+ 
+ 
+  private
+ 
+   #def set_item
+    # @items = Item.find(params[:id])
+   #end
+end
