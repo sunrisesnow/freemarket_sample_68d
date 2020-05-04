c4fc60c9d5d63e46280e0e6f5276b62b2f1c2a9e
MugiOka <okamugi0722@gmail.com>
2020-05-04 20:12:28 +0900

    ビューの記載/ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tops, only: [:new]
-  resources :items
+  resources :items do
+    collection do
+      get 'category_children', defaults: { format: 'json' }
+      get 'category_grandchildren', defaults: { format: 'json' }
+    end
+  end
   resources :categories, only: [:index]
 end
