c341b2c2a5c24acf97aef5e3c3c5df69994273e8
MugiOka <okamugi0722@gmail.com>
2020-05-05 22:14:24 +0900

    クリックしたカテゴリー要素（親、子、孫）別にうまく取得したい商品を取れるようにアクションを作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,44 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:parent, :child]
   def index
     @parents = Category.where(ancestry: nil)
     @brands = ["シャネル","ナイキ", "ルイヴィトン", "シュプリーム","アディダス"]
+  end
+
+  def parent
+    #親要素の子要素代入
+    children = @category.children
+    grandchildren = []
+    # ancestryに(親要素id/子要素id)が入っている要素全て取得
+    children.each do |child|
+      grandchildren << Category.where(ancestry: "#{@category.id}/#{child.id}")
+    end
+    @items = []
+    # grandchildrenは（１子要素の１孫要素、２子要素の２孫要素）という多重配列なのでeachを二回重ねることで親要素の全ての孫要素のidをwhereメソッドに渡しています。
+    grandchildren.each do |grandchild|
+      grandchild.each do |id|
+        @items += Item.where(category_id: id)
+      end
+    end
+  end
+
+  def child
+    #子要素の孫要素代入
+    grandchildren = @category.children
+    @items = []
+    #子要素の孫要素という一つの配列なのでeachは一度で孫要素のid取得
+    grandchildren.each do |grandchild|
+      @items += Item.where(category_id: grandchild.id)
+    end
+  end
+
+  def grandchild
+    @items = Item.where(category_id: params[:id])
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
