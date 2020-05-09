b93adc536321729673c7df4134245324c1084d7c
MugiOka <okamugi0722@gmail.com>
2020-05-10 01:03:45 +0900

    クレジットカードのコントローラー

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,78 +4,73 @@ class CardsController < ApplicationController
   
   def index
     if @card.present?
-      # 登録している場合,PAY.JPからカード情報を取得する
+      # カードがある場合
       # PAY.JPの秘密鍵をセットする。
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      # PAY.JPから顧客情報を取得する。
+      # customerにはretrieveメソッドの引数に指定したidに一致する顧客情報を取得。
       customer = Payjp::Customer.retrieve(@card.payjp_id)
-      # PAY.JPの顧客情報から、デフォルトで使うクレジットカードを取得する。
+      # カード情報はdefault_cardメソッドを使うことで取得できる。
       @card_info = customer.cards.retrieve(customer.default_card)
-      # クレジットカード情報から表示させたい情報を定義する。
-      # クレジットカードの画像を表示するために、カード会社を取得
+      # カードの親会社名を取得
       @card_brand = @card_info.brand
       # クレジットカードの有効期限を取得
       @exp_month = @card_info.exp_month.to_s
       @exp_year = @card_info.exp_year.to_s.slice(2,3) 
+    else
+      render :new
     end
   end
 
   def new
     # cardがすでに登録済みの場合、indexのページに戻します。
-    @card = Card.where(user_id: current_user.id).first
+    @card = Card.find_by(user_id: current_user.id)
     redirect_to action: "index" if @card.present?    
   end
 
   def create
     # PAY.JPの秘密鍵をセット（環境変数）
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-
     # jsで作成したpayjpTokenがちゃんと入っているか？
     if params['payjpToken'].blank?
-      render "new"
+      render :new
     else
       # トークンがちゃんとあれば進めて、PAY.JPに登録されるユーザーを作成します。
       customer = Payjp::Customer.create(
-        description: 'test',
-        email: current_user.email,
-        card: params['payjpToken'],
-        metadata: {user_id: current_user.id}
+        description: 'test',#ここの記述はなくてもよし
+        email: current_user.email,#ここの記述はなくてもよし
+        card: params['payjpToken'],#トークンだけ送るだけでも可能
+        metadata: {user_id: current_user.id},#ここの記述はなくてもよし
       )
 
       # PAY.JPのユーザーが作成できたので、Cardモデルを登録します。
       @card = Card.new(user_id: current_user.id, payjp_id: customer.id)
-      if @card.save
-        redirect_to action: "index", notice:"支払い情報の登録が完了しました"
+      if @card.save!
+        redirect_to action: "index"
       else
-        render 'new'
+        render :new
       end
     end
   end
 
   def destroy     
-    # 今回はクレジットカードを削除するだけでなく、PAY.JPの顧客情報も削除する。これによりcreateメソッドが複雑にならない。
-    # PAY.JPの秘密鍵をセットして、PAY.JPから情報をする。
+    # PAY.JPの秘密鍵をセットして、PAY.JPから情報を取得。
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    # PAY.JPの顧客情報を取得
+    # retrieveメソッドを使うことで引数にしていされているidと一致する顧客情報を取得
+    # PAY.JPの顧客情報を削除
     customer = Payjp::Customer.retrieve(@card.payjp_id)
-    customer.delete # PAY.JPの顧客情報を削除
-    if @card.destroy # App上でもクレジットカードを削除
-      redirect_to action: "index", notice: "削除しました"
-    else
-      redirect_to action: "index", alert: "削除できませんでした"
-    end
+    customer.delete 
+    # App上でもクレジットカードを削除
+    redirect_to action: "index" if @card.destroy 
   end
 
   def buy
     @item = Item.find(params[:id])
     # すでに購入されていないか？
     if @item.buyer_id.present? 
-      flash[:alert] = 'すでに購入されてしまいました。'
       render root_path
     elsif @card.blank?
       # カード情報がなければ、買えないから戻す
-      flash[:alert] = '購入にはクレジットカード登録が必要です。\nクレジットカード登録ページにて登録を済ませてください。'
-      render "new"
+      render :new
     else
       # 購入者もいないし、クレジットカードもあるし、決済処理に移行
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
@@ -87,11 +82,7 @@ class CardsController < ApplicationController
       )
       # 売り切れなので、Itemの情報をアップデートして売り切れにします。
       if @item.update(buyer_id: current_user.id)
-        flash[:notice] = '購入しました。'
-        redirect_to controller: 'items', action: 'show', id: @item.id
-      else
-        flash[:alert] = '購入に失敗しました。'
-        redirect_to controller: 'items', action: 'show', id: @item.id
+        redirect_to controller: 'items', action: 'show', id: @item.id 
       end
     end
   end
