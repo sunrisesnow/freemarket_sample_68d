dfdbcccd771b74e22c7068801b661e3528d9e8f2
MugiOka <okamugi0722@gmail.com>
2020-05-11 18:55:38 +0900

    フェードインイベントの実装
    トップページなどの商品一覧ページの商品に少し遊びを入れました

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -45,4 +45,15 @@ $(function () {
 
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
+
+  $(window).scroll(function (){
+    $('.pickup-box').each(function(){
+      var position = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > position - windowHeight + 100){
+        $(this).addClass('active');
+      }
+    });
+  });
 });
