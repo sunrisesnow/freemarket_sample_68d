f8d0a3cd07c891a4399484f494bd1d65cab45d6f
m.teshita <masato.t2207@gmail.com>
2020-05-14 18:20:06 +0900

    n+1問題の解消のために、includesの記述を削除

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,7 +1,7 @@
 class TopsController < ApplicationController
   skip_before_action :authenticate_user!
   def index  
-    @items = Item.includes(:images).limit(5).order('created_at DESC')
+    @items = Item.limit(5).order('created_at DESC')
     @parents = Category.where(ancestry: nil)
     @brands = ["シャネル","ナイキ", "ルイヴィトン", "シュプリーム","アディダス"]
   end
