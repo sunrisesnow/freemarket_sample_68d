8080d354d98a4a6acd43d89b2666e8f247c3c016
MugiOka <okamugi0722@gmail.com>
2020-05-15 17:25:54 +0900

    不要記述の削除/リファクタリング

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,10 +10,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def create
     @user = User.new(sign_up_params)
-    unless @user.valid?
-      flash.now[:alert] = @user.errors.full_messages
-      render :new and return
-    end
+    render :new and return unless @user.valid?
     session["devise.regist_data"] = {user: @user.attributes}
     session["devise.regist_data"][:user]["password"] = params[:user][:password]
     @address = @user.build_address
@@ -23,10 +20,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create_address
     @user = User.new(session["devise.regist_data"]["user"])
     @address = Address.new(address_params)
-    unless @address.valid?
-      flash.now[:alert] = @address.errors.full_messages
-      render :new_address and return
-    end
+    render :new_address and return unless @address.valid?
     @user.build_address(@address.attributes)
     @user.save
     session["devise.regist_data"]["user"].clear
