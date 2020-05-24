22f34f2b60ce6c2447d48827184735e28c7f89ba
MugiOka <okamugi0722@gmail.com>
2020-05-25 06:12:38 +0900

    likesコントローラーの記載
    create, destroyは非同期通信で行っています。

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,29 @@
+class LikesController < ApplicationController
+  before_action :set_item, only: [:create, :destroy]
+
+  def index
+    @parents = Category.where(ancestry: nil)
+    @brands = ["シャネル","ナイキ", "ルイヴィトン", "シュプリーム","アディダス"]
+    items = []
+    likes = Like.where(user_id: params[:item_id])
+    if likes.present?
+      likes.each { |like| items << Item.find(like.item_id)}
+    end
+    @items = Kaminari.paginate_array(items).page(params[:page]).per(15)
+  end
+
+  def create
+    like = Like.create(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def destroy
+    like = Like.find(params[:id])
+    current_user.id == like.user_id ? ( like.destroy) : (redirect_to root_path)
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+end
