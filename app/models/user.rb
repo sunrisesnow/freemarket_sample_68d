8686d61e87c4a936f0b0e8fe6d46187c4b1fdd57
MugiOka <okamugi0722@gmail.com>
2020-05-25 18:35:02 +0900

    不要記述の削除、記述漏れの補填

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
   has_one :address, dependent: :destroy
   has_one :card, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
   
 
   with_options presence: true do
