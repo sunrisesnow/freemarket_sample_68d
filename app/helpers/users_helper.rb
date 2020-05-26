c942ce5f7963f7c486d6a04892fe13774c1e0f29
MugiOka <okamugi0722@gmail.com>
2020-05-27 00:40:24 +0900

    ビューの修正
    ・ルーティングのネストを変更したためpathの修正
    ・ビューに条件分岐が多いと感じたため、ヘルパーフォルダのuser_helperのメソッドを定義しビューで使用。

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,41 @@
 module UsersHelper
+  def header_icon_image_present?(account)
+    if account.present? && account.icon_image.present?
+      image_tag account.icon_image.url, class: "user__mypage--icon"
+    else
+      image_tag "member_photo_noimage.png", class: "user__mypage--icon"
+    end
+  end
+
+  def tab_icon_image_present?(account)
+    if account.present? && account.icon_image.present?
+      image_tag account.icon_image.url, class: "user__mypage--big--icon"
+    else
+      image_tag "member_photo_noimage.png", class: "user__mypage--big--icon"
+    end
+  end
+
+  def tab_background_image_present?(account)
+    if account.present? && account.background_image.present?
+      image_tag account.background_image.url, class: "mypage__background"
+    else
+      image_tag "user-background.jpg", class: "mypage__background"
+    end
+  end
+
+  def mypage_icon_image_present?(account)
+    if account.present? && account.icon_image.present?
+      image_tag account.icon_image.url, class: "mypage-user-icon-image"
+    else
+      image_tag "member_photo_noimage.png", class: "mypage-user-icon-image"
+    end
+  end
+
+  def mypage_background_image_present?(account)
+    if account.present? && account.background_image.present?
+      image_tag account.background_image.url, class: "content_image"
+    else
+      image_tag "user-background.jpg", class: "content_image" 
+    end
+  end
 end
