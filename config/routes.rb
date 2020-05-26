9200d6c5dd5771f850973d228d0a35c561b7ff8a
m.teshita <masato.t2207@gmail.com>
2020-05-26 18:57:28 +0900

    itemsコントローラーにsearchアクションのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
     collection do
       get 'category_children', defaults: { format: 'json' }
       get 'category_grandchildren', defaults: { format: 'json' }
+      get 'search'
     end
   end
 end
