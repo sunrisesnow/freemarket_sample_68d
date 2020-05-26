e37c2715472b471c14b489ec8f7dc147aaee39b3
MugiOka <okamugi0722@gmail.com>
2020-05-27 04:09:29 +0900

    items_controllerの記述の追加と修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :set_category_brand, only: [:index, :new, :create, :show, :edit, :update]
+  before_action :set_category_brand,  except: [:destroy]
 
   def index
-    @items = Item.includes(:images).order('created_at DESC')
+    @items = Item.includes(:images).order('created_at DESC').where.not(trading_status_id: 4)
   end
 
   def new
@@ -27,13 +27,14 @@ class ItemsController < ApplicationController
       params[:item_images][:image].each do |image|
         @item.images.create(image: image, item_id: @item.id)
       end
-      redirect_to items_path
+      @item.trading_status_id == 4 ? (redirect_to draft_items_path) : (redirect_to items_path)
     else
       render :new
     end
   end
 
   def edit
+    redirect_to root_path unless current_user.id == @item.saler_id
     @item.images.all
     @parents = []
     Category.where(ancestry: nil).each do |parent|
@@ -60,7 +61,7 @@ class ItemsController < ApplicationController
           @item.images.create(image: image, item_id: @item.id) if @item.images.count <= 10
         end
       end
-      redirect_to item_path
+      @item.trading_status_id == 4 ? (redirect_to draft_items_path) : (redirect_to item_path(@item))
     else
       render :edit
     end
@@ -68,12 +69,28 @@ class ItemsController < ApplicationController
 
   def show
     @user = User.find_by(id: @item.saler_id)
-    redirect_to root_path if @item == nil
+    redirect_to root_path if @item == nil || @item.trading_status_id != 1
   end 
 
   def destroy
     redirect_to root_path  unless current_user.id == @item.saler_id
     @item.destroy ? (redirect_to root_path) : (redirect_to item_path(@item)) 
+  end
+
+  def draft
+    @items = Item.includes(:images).where(trading_status_id: 4).where(saler_id: current_user.id).page(params[:page]).per(15)
+  end
+
+  def exhibition
+    @items = Item.includes(:images).where(saler_id: current_user.id).where(buyer_id: nil).where(trading_status_id: 1).page(params[:page]).per(15)
+  end
+
+  def exhibition_trading
+    @items = Item.includes(:images).where(saler_id: current_user.id).where.not(buyer_id: nil).page(params[:page]).per(15)
+  end
+
+  def bought
+    @items = Item.includes(:images).where(buyer_id: current_user.id).page(params[:page]).per(15)
   end
 
   private
