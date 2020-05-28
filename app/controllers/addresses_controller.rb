bc22fa27d6e0510fccdf94bb95875d3240532958
MugiOka <okamugi0722@gmail.com>
2020-05-28 16:21:31 +0900

    addresses_controllerの記述

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,29 @@
+class AddressesController < ApplicationController
+  before_action :set_category_brand
+  before_action :set_address, except: [:show]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @address.update!(address_params) ? (redirect_to edit_address_path(@address)) : (render :edit)
+  end
+
+  private
+
+  def set_category_brand
+    @parents = Category.where(ancestry: nil)
+    @brands = ["シャネル","ナイキ", "ルイヴィトン", "シュプリーム","アディダス"]
+  end
+
+  def set_address
+    @address = Address.find_by(user_id: current_user.id)
+  end
+
+  def address_params
+    params.require(:address).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :prefectures, :postal_code, :municipality, :address, :phone_number, :building)
+  end
+end
