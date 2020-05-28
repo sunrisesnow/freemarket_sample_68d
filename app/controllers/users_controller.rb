35d7058aa922e48b40785c31b5b69789ee0f5f45
MugiOka <okamugi0722@gmail.com>
2020-05-28 16:23:28 +0900

    users_controllerの記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
 
+  before_action :set_category_brand
+
   def show
-    @parents = Category.where(ancestry: nil)
-    @brands = ["シャネル","ナイキ", "ルイヴィトン", "シュプリーム","アディダス"]
   end
 
+  def index
+  end
+
+  private
+
+  def set_category_brand
+    @parents = Category.where(ancestry: nil)
+    @brands  =  ["シャネル","ナイキ", "ルイヴィトン", "シュプリーム","アディダス"]
+  end
 end
