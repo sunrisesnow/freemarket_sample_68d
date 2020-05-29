bb94d5ff6e8f66cf2492673504e1a39ae93a7683
MugiOka <okamugi0722@gmail.com>
2020-05-29 09:04:24 +0900

    不要js削除

--- app/assets/javascripts/account.js
+++ app/assets/javascripts/account.js
@@ -1,25 +1,4 @@
 $(function(){
-  $(document).on('change', '#icon_image', function () {
-    const file = this.files[0];
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(file);
-    fileReader.onload = function () {
-      const image = this.result;
-      $('.account__image__form__icon').attr({ src: image });
-      $('.user__mypage--icon').attr({ src: image });
-      $('.user__mypage--big--icon').attr({ src: image });
-    }
-  });
-  $(document).on('change',  '#background_image', function () {
-    const file = this.files[0];
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(file);
-    fileReader.onload = function () {
-      const image = this.result;
-      $('.account__image__form__background').attr({ src: image });
-      $('.mypage__background').attr({ src: image });
-    }
-  });
   $(document).on('change', '#account_icon_image', function () {
     const file = this.files[0];
     const fileReader = new FileReader();
