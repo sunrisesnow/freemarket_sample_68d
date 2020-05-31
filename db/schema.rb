4abfc78d8deb89a6eeb49d1103987261d164803d
MugiOka <okamugi0722@gmail.com>
2020-05-31 16:30:15 +0900

    スキーマの更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_102712) do
+ActiveRecord::Schema.define(version: 2020_05_30_231226) do
 
   create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -92,6 +92,18 @@ ActiveRecord::Schema.define(version: 2020_05_26_102712) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "from_id", null: false
+    t.bigint "to_id", null: false
+    t.bigint "room_id", null: false
+    t.text "message", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["from_id"], name: "index_messages_on_from_id"
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["to_id"], name: "index_messages_on_to_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -117,4 +129,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_102712) do
   add_foreign_key "items", "categories"
   add_foreign_key "likes", "items"
   add_foreign_key "likes", "users"
+  add_foreign_key "messages", "items", column: "room_id"
+  add_foreign_key "messages", "users", column: "from_id"
+  add_foreign_key "messages", "users", column: "to_id"
 end
