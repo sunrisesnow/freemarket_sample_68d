d0fc4b4e746167781f4995bc65bd34a1d821ab30
MugiOka <okamugi0722@gmail.com>
2020-06-02 00:13:25 +0900

    trading_controllerの修正（詳細あり）
    ・trading_helperのインクルード
    ・before_actionの追加
    ・条件多岐のif文をcase文に変更（見にくいかな？笑）

--- app/controllers/trading_controller.rb
+++ app/controllers/trading_controller.rb
@@ -1,23 +1,45 @@
 class TradingController < ApplicationController
+  include TradingHelper
   before_action :set_category_brand
   before_action :set_trading_item
   before_action :set_item_search_query
+  before_action :set_trading_item_users
+  before_action :set_new_message
   before_action :item_user?
   
   def show
-    @message  = Message.new
     @messages = @item.messages.includes(:from).order('created_at ASC')
+    trading_item_fee(@item)
   end
 
   def update
-    if @item.trading_status_id == 1 && current_user.id == @item.saler_id
-      @item.update!(trading_status_id: 2) ? (redirect_to item_trading_path(@item, current_user)) : (redirect_to root_path)
-    elsif @item.trading_status_id == 2  && current_user.id == @item.buyer_id
-      @item.update!(trading_status_id: 3) ? (redirect_to item_trading_path(@item, current_user)) : (redirect_to root_path)
-    elsif @item.trading_status_id == 3  && current_user.id == @item.saler_id
-      @item.update!(trading_status_id: 5) ? (redirect_to item_trading_path(@item, current_user)) : (redirect_to root_path)
-    else
-      redirect_to root_path
+    case current_user.id
+    when @item.saler_id
+      case @item.trading_status_id
+      when 1
+        redirect_to root_path unless @item.update!(trading_status_id: 2) 
+      when 3
+        redirect_to root_path unless @item.update!(trading_status_id: 5)
+      else
+        redirect_to root_path
+      end
+    when @item.buyer_id
+      case @item.trading_status_id
+      when 2 
+        redirect_to root_path unless @item.update(trading_status_id: 3)
+      else
+        redirect_to root_path
+      end
     end
+  end
+
+  private
+
+  def set_trading_item_users
+    trading_item_users(@item)
+  end
+
+  def set_new_message
+    @message  = Message.new
   end
 end
