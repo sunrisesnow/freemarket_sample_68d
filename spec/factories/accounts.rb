26865282e7af9ecbc7623c0cf70a6c3490e55ba3
MugiOka <okamugi0722@gmail.com>
2020-06-03 08:28:40 +0900

    AccountモデルRspecテスト

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
-  factory :account do
-    
+  factory :account, class: Account do
+    icon_image       {"test.jpg"}
+    background_image {"test.jpg"}
+    introduction     {"よろしく"}
+  end
+
+  factory :not_account, class: Account do
   end
 end
+
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Account do
+  describe "#create" do
+    let!(:user){create(:user)}
+    context "can save" do
+      it "user_idが存在すれば項目が全てみたされていても登録できる事" do
+        account = build(:account, user_id: user.id)
+        expect(account).to be_valid
+      end
+      it "user_idが存在すれば項目が全て未入力でも登録できる事" do
+        account = build(:not_account, user_id: user.id)
+        expect(account).to be_valid
+      end
+    end
+    context "can not save" do
+      it "user_idが存在しない場合登録できない事" do
+        account = build(:account,user_id: nil)
+        account.valid?
+        expect(account.errors[:user_id]).to include("を入力してください")
+      end
+    end
+  end
 end
