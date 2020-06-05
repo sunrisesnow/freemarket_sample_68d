0c1545f81c6c2f0eec049011942ad29624f63f25
m.teshita <masato.t2207@gmail.com>
2020-06-05 20:47:41 +0900

    詳細検索条件で、価格帯の選択をやめた時の挙動を追加

--- app/assets/javascripts/item_search.js
+++ app/assets/javascripts/item_search.js
@@ -1,3 +1,4 @@
+// 並び替えの挙動
 $(function() {
   // プルダウンメニューを選択することでイベントが発生
   $('select[name=sort_order]').change(function() {
@@ -44,13 +45,16 @@ $(function() {
       $(add_selected).attr('selected', true)
     }
   });
+});
+
+// 検索条件の挙動
+$(function(){
+  const min_price = $('#q_price_gteq');
+  const max_price = $('#q_price_lteq');
 
   // 価格帯を選択したら、minとmaxに値を
   $('#price_range').on('change', function() {
     const price_range = $(this).val();
-    const min_price = $('#q_price_gteq')
-    const max_price = $('#q_price_lteq')
-
     if (price_range != "") {
       $.ajax({
         url: '/items/price_range',
@@ -65,87 +69,9 @@ $(function() {
       .fail(function() {
         alert('価格帯の取得に失敗しました')
       })
-    }
-    
-  });
-
-  // 検索条件のチェックボックスの挙動
-  const checkbox_all_select = $('.js-checkbox-all');
-  const status_all_checkbox = $('#status_all');
-  const status_checkboxes = $('input[name="q[status_id_in][]"]')
-  const delivery_charge_all_checkbox = $('#delivery_charge_flag_all')
-  const delivery_charge_checkboxes = $('input[name="q[delivery_charge_flag_in][]"]')
-  const trading_status_all_checkbox = $('#trading_status_all')
-  const trading_status_checkboxes = $('input[name="q[trading_status_id_in][]"]')
-  
-  // 「すべて」をクリックした時の挙動
-  function targetCheckboxesChage(target, trigger) {
-    if (trigger.prop("checked") == true) {
-      target.prop("checked", true);
     } else {
-      target.prop("checked", false);
+      min_price.val('');
+      max_price.val('');
     }
-  }
-
-  // 「すべて」以外をクリックした時の挙動
-  function allCheckboxChange(target, all_checkbox, trigger) {
-    if (trigger.prop("checked") == false) {
-      all_checkbox.prop("checked", false);
-    } else {
-      let flag = true
-      target_checkboxes.each(function(e) {
-        if (target_checkboxes.eq(e).prop("checked") == false) {
-          flag = false;
-        }
-      });
-      if (flag) {
-        all_checkbox.prop("checked", true);
-      }
-    }
-  }
-
-  $('.js_search_checkbox-all > input:checkbox').on('change', function() {
-    let target_checkboxes;
-    switch ($(this).prop('id')) {
-      case status_all_checkbox.prop('id'):
-        target_checkboxes = status_checkboxes;
-        break;
-      case delivery_charge_all_checkbox.prop('id'):
-        target_checkboxes = delivery_charge_checkboxes;
-        break;
-      case trading_status_all_checkbox.prop('id'):
-        target_checkboxes = trading_status_checkboxes;
-        break;
-      default: ;
-    }
-    targetCheckboxesChage(target_checkboxes, $(this));
-  });
-
-  $('.js_search_checkbox > input:checkbox').on('change', function() {
-    let all_checkbox;
-    switch ($(this).prop('name')) {
-      case status_checkboxes.prop('name'):
-        target_checkboxes = status_checkboxes;
-        all_checkbox = status_all_checkbox;
-        break;
-      case delivery_charge_checkboxes.prop('name'):
-        target_checkboxes = delivery_charge_checkboxes;
-        all_checkbox = delivery_charge_all_checkbox;
-        break;
-      case trading_status_checkboxes.prop('name'):
-        target_checkboxes = trading_status_checkboxes;
-        all_checkbox = trading_status_all_checkbox;
-        break;
-      default: ;
-    }
-    allCheckboxChange(target_checkboxes, all_checkbox, $(this));
-  });
-
-  // クリアボタン押下で条件を全クリア
-  $('#js_conditions_clear').click(function(e) {
-    checkbox_all_select.prop("checked", false);
-    status_checkboxes.prop("checked", false);
-    delivery_charge_checkboxes.prop("checked", false);
-    trading_status_checkboxes.prop("checked", false);
   });
 });
