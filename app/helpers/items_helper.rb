b7653b6cfe9f2047fb6f41290b4fcf738e9f0214
m.teshita <masato.t2207@gmail.com>
2020-06-06 16:30:21 +0900

    refactor:商品のビューファイルの記述を簡略化するためのヘルパーメソッド定義

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,24 @@
 module ItemsHelper
+  def delivery_charge(item)
+    case item.delivery_charge_flag 
+      when 1
+        "送料込み"
+      when 2
+        "着払い"
+      else
+        "配送料の負担が選択されていません"
+    end
+  end
+
+  def delivery_charge_full(item)
+    case item.delivery_charge_flag 
+      when 1
+        "送料込み(出品者負担)"
+      when 2
+        "着払い(購入者負担)"
+      else
+        "配送料の負担が選択されていません"
+    end
+  end
+
 end
