2d404cda33bc6a89c85bcbf179965438294c0d23
MugiOka <okamugi0722@gmail.com>
2020-06-07 20:40:31 +0900

    fix: Evaluationモデル/validates

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -3,5 +3,10 @@ class Evaluation < ApplicationRecord
   belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User"
 
-  validates :evaluation, presence: true
+  with_options presence: true do
+    validates :user_id
+    validates :saler_id
+    validates :buyer_id
+    validates :evaluation
+  end
 end
