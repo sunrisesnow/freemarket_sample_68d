f135a1975478d09a2d734157d354413b3154ea17
m.teshita <masato.t2207@gmail.com>
2020-06-14 15:39:06 +0900

    refacor: アクションを分類して並び替え

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,83 +4,12 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :set_categories,  except: [:destroy]
   
+# 未ログインで行えるアクション
   def index
   end
 
-  def new
-    @item = Item.new
-    @item.images.new
-    @parents = Category.ancestries(nil).name_not("カテゴリー一覧")
-  end
-
-  def category_children
-    @children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
-  end
-
-  def category_grandchildren
-    @grandchildren = Category.find("#{params[:child_id]}").children
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save!
-      params[:item_images][:image].each do |image|
-        @item.images.create(image: image, item_id: @item.id)
-      end
-      redirect_to draft_items_path if @item.trading_status_id == 4
-    else
-      render :new
-    end
-  end
-
-  def edit
-    redirect_to root_path unless current_user.id == @item.saler_id
-    @parents = Category.ancestries(nil).name_not("カテゴリー一覧")
-    @category_child_array = @item.category.parent.siblings
-    @category_grandchild_array = @item.category.siblings
-    @delivery_methods = DeliveryMethod.find_all_by_flag(@item.delivery_charge_flag.to_s)
-  end
-
-  def update
-    if @item.update!(item_params)
-      if add_item_images = params[:item][:image]
-        add_item_images.each do|image|
-          @item.images.create(image: image, item_id: @item.id) if @item.images.count <= 10
-        end
-      end
-      @item.trading_status_id == 4 ? (redirect_to draft_users_path) : (redirect_to item_path(@item))
-    else
-      render :edit
-    end
-  end
-
   def show
-    if @item == nil || @item.trading_status_id == 4
-      redirect_to root_path 
-    else
-      @user = User.find_by(id: @item.saler_id)
-    end
-  end 
-
-  def destroy
-    redirect_to root_path  unless current_user.id == @item.saler_id
-    if @item.trading_status_id == 5
-      @item.destroy ? (redirect_to exhibition_completed_users_path) : (redirect_to item_trading_path(@item, current_user)) 
-      return
-    end
-    @item.destroy && @item.trading_status_id == 4 ? (redirect_to draft_users_path) : (redirect_to exhibition_users_path) 
-  end
-
-  def delivery_method
-    @delivery_method = DeliveryMethod.find_all_by_flag(params[:flag])
-  end
-
-  def price_range
-    if params[:price_id].nil?
-      @price_range = PriceRange.find_by_min_and_max(params[:min], params[:max])
-    else
-      @price_range = PriceRange.find(params[:price_id])
-    end
+    @item.trading_status_id == 4 ? (redirect_to root_path) : (@user = User.find_by(id: @item.saler_id))
   end
 
   def search
@@ -137,6 +66,76 @@ class ItemsController < ApplicationController
       end
     end
     @items = Kaminari.paginate_array(@items).page(params[:page]).per(20)
+  end
+
+# ログインしないと行えないアクション
+  def new
+    @item = Item.new
+    @item.images.new
+    @parents = Category.ancestries(nil).name_not("カテゴリー一覧")
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save!
+      params[:item_images][:image].each do |image|
+        @item.images.create(image: image, item_id: @item.id)
+      end
+      redirect_to draft_items_path if @item.trading_status_id == 4
+    else
+      render :new
+    end
+  end
+
+  def edit
+    redirect_to root_path unless current_user.id == @item.saler_id
+    @parents = Category.ancestries(nil).name_not("カテゴリー一覧")
+    @category_child_array = @item.category.parent.siblings
+    @category_grandchild_array = @item.category.siblings
+    @delivery_methods = DeliveryMethod.find_all_by_flag(@item.delivery_charge_flag.to_s)
+  end
+
+  def update
+    if @item.update!(item_params)
+      if add_item_images = params[:item][:image]
+        add_item_images.each do|image|
+          @item.images.create(image: image, item_id: @item.id) if @item.images.count <= 10
+        end
+      end
+      @item.trading_status_id == 4 ? (redirect_to draft_users_path) : (redirect_to item_path(@item))
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    redirect_to root_path  unless current_user.id == @item.saler_id
+    if @item.trading_status_id == 5
+      @item.destroy ? (redirect_to exhibition_completed_users_path) : (redirect_to item_trading_path(@item, current_user)) 
+      return
+    end
+    @item.destroy && @item.trading_status_id == 4 ? (redirect_to draft_users_path) : (redirect_to exhibition_users_path) 
+  end
+
+  # ajax通信でデータ取得するためのメソッド
+  def category_children
+    @children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  def category_grandchildren
+    @grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
+  def delivery_method
+    @delivery_method = DeliveryMethod.find_all_by_flag(params[:flag])
+  end
+
+  def price_range
+    if params[:price_id].nil?
+      @price_range = PriceRange.find_by_min_and_max(params[:min], params[:max])
+    else
+      @price_range = PriceRange.find(params[:price_id])
+    end
   end
 
   private
