210be28a14b72850565e101fca2c899427ee54f9
m.teshita <masato.t2207@gmail.com>
2020-06-14 21:20:18 +0900

    fix: ログイン不要なURLの場合にサインイン画面に遷移しないように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :redirect_to_root, only: [:render_404]
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
@@ -29,6 +30,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def redirect_to_root
+    redirect_to root_path
   end
 
   def set_item_search_query
