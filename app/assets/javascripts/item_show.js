129c4ccae95400caae858f1a7452f861e89308b3
m.teshita <masato.t2207@gmail.com>
2020-06-17 14:42:29 +0900

    feat: モーダル表示するjsの記述

--- app/assets/javascripts/item_show.js
+++ app/assets/javascripts/item_show.js
@@ -41,4 +41,38 @@ $(function() {
     });
     $(thumbnailImages+'[data-index="'+nextSlide+'"]').addClass("thumbnail-current");
   });
+  
+  // モーダルの挙動
+  $('[data-modal="overlay"], [data-modal="content"]').hide();
+  
+  const modal_slider = "#slider_images_modal"
+
+  $(sliderImages).on('click', function() {
+    index = Number($(this).attr("data-slick-index"))
+    $(modal_slider).slick({
+      prevArrow: '<i class="fa fa-angle-left arrow arrow-left"></i>',
+      nextArrow: '<i class="fa fa-angle-right arrow arrow-right"></i>',
+      dots: true,
+      speed: 800,
+      initialSlide: index
+    });
+    posi = $(window).scrollTop();
+    $('[data-modal="fixed"]').css({
+      position: 'fixed',
+      top: -1 * posi
+    });
+    $('[data-modal="overlay"], [data-modal="content"]').fadeIn();
+    $(modal_slider).css('opacity',0);
+    $(modal_slider).animate({'z-index':1},500,function(){
+      $(modal_slider).slick('setPosition');
+      $(modal_slider).animate({'opacity':1});
+    });
+  });
+  
+  $('.js_modal_close, [data-modal="overlay"]').on('click', function(){
+    $('[data-modal="fixed"]').attr('style', '');
+    $('html, body').prop({scrollTop: posi});
+    $('[data-modal="overlay"], [data-modal="content"]').fadeOut();
+    $(modal_slider).slick('unslick')
+  });
 });
