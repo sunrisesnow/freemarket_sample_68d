6512a68968c7eb6087fb6dcba259cfc1489836c5
sega16322 <aventador_849@docomo.ne.jp>
2020-06-20 19:24:41 +0900

    不要なマイグレーションファイルの消去・rails db:migrate:reset

--- db/migrate/20200618215024_change_data_checked_to_notifications.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDataCheckedToNotifications < ActiveRecord::Migration[5.2]
-  def up
-    change_column :notifications, :checked, :boolean, default: false, null: false
-  end
-
-  def down
-    change_column :notifications, :checked, :boolean, null: true
-  end
-end
