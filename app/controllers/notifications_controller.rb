8c9e54d66116a4328a969077bef0ebbee75688f5
岡麦 <okamugi0722@gmail.com>
2020-06-24 18:46:49 +0900

    refactor: 不要記述削除

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,16 +1,11 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_category_brand
+  before_action :set_categories
   before_action :set_item
   before_action :set_item_search_query
   before_action :my_notifications
 
   private
-  def set_category_brand
-    @parents = Category.where(ancestry: nil)
-    @brands = ["シャネル","ナイキ", "ルイヴィトン", "シュプリーム","アディダス"]
-  end
-
   def set_item
     @item = Item.find_by_id(params[:id])
   end
